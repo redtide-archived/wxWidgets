8f3fbfbe9bc2ec5ff156a0d5b1e6b9fc694028e0
AZ <wxguidesigner@gmail.com>
2012-12-19 02:46:31 +0100

    Committed WIP

--- build/premake/wx.lua
+++ build/premake/wx.lua
@@ -8,84 +8,16 @@
 -- Copyright:   (c) Andrea Zanellato <widgets.wx@gmail.com>
 -- Licence:     wxWindows licence
 -- ============================================================================
+
 wx = {}
 
-wx.version          = "2.9.5"
-wx.compiler         = ""
-wx.includedir       = ""
-wx.libdir           = ""
-wx.static           = false
--- ----------------------------------------------------------------------------
--- X Features
--- ----------------------------------------------------------------------------
-wx.x-includes       = ""
-wx.x-libraries      = ""
--- ----------------------------------------------------------------------------
--- Global build options
--- ----------------------------------------------------------------------------
-wx.gui              = true
-wx.monolithic       = false
-wx.plugins          = false
-wx.subdirs          = false
-wx.flavour          = ""
-wx.official_build   = false
-wx.vendor           = "custom"
-wx.all-features     = true
--- ----------------------------------------------------------------------------
--- Port selection
--- ----------------------------------------------------------------------------
-wx.gtk              = 0
-wx.motif            = 0
-wx.osx_carbon       = 0
-wx.osx_cocoa        = 0
-wx.osx_iphone       = 0
-wx.osx              = 0
-wx.carbon           = wx.osx_carbon
-wx.cocoa            = wx.osx_cocoa
-wx.iphone           = wx.osx_iphone
-wx.mac              = wx.osx
-wx.old_cocoa        = 0
-wx.wine             = 0
-wx.msw              = 0
-wx.pm               = 0
-wx.directfb         = 0
-wx.microwin         = 0
-wx.x11              = 0
-wx.nanox            = 0
-wx.gpe              = 0
-wx.universal        = false
-wx.themes           = "all"
--- ----------------------------------------------------------------------------
--- External libraries
--- ----------------------------------------------------------------------------
-wx.libpng           = 0
-wx.libjpeg          = 0
-wx.libtiff          = 0
-wx.libxpm           = 0
-wx.libiconv         = 0
-wx.libmspack        = 0
-wx.gtkprint         = 0
-wx.gnomeprint       = 0
-wx.gnomevfs         = 0
-wx.libnotify        = 0
-wx.hildon           = 0
-wx.opengl           = 0
-
-wx.dmalloc          = 0
-wx.sdl              = 0
-wx.regex            = 0
-wx.zlib             = 0
-wx.expat            = 0
-
-wx.macosx-sdk         = ""
-wx.macosx-version-min = ""
 -- ---------------------------------------------------------------------------
--- Debugging options
+-- TODO: Temporary options
 -- ---------------------------------------------------------------------------
-wx.debug            = false
-wx.debug_flag       = false
-wx.debug_info       = false
-wx.debug_gdb        = false
+wx.version                      = "2.9.5"
+wx.compiler                     = ""
+wx.includedir                   = ""
+wx.libdir                       = ""
 -- ---------------------------------------------------------------------------
 -- Global compile options
 -- ---------------------------------------------------------------------------
@@ -101,3 +33,337 @@ wx.utf8                         = false
 wx.utf8only                     = false
 wx.extended_rtti                = false
 wx.omf                          = false
+wx.optimize                     =
+wx.profile                      =
+wx.no_rtti                      =
+wx.no_exceptions                =
+wx.permissive                   =
+wx.no_deps                      =
+wx.vararg_macros                = true
+wx.universal_binary             = false
+wx.macosx_arch                  = ""
+wx.compat26                     = false
+wx.compat28                     = true
+wx.rpath                        = true
+wx.objc_uniquifying             =
+wx.visibility                   =
+wx.tls                          =
+-- ----------------------------------------------------------------------------
+-- Global build options
+-- ----------------------------------------------------------------------------
+wx.gui                          = true
+wx.monolithic                   = false
+wx.plugins                      = false
+wx.subdirs                      = false
+wx.flavour                      = ""
+wx.official_build               = false
+wx.vendor                       = "custom"
+wx.all-features                 = true
+-- ----------------------------------------------------------------------------
+-- X Features
+-- ----------------------------------------------------------------------------
+wx.x-includes                   = ""
+wx.x-libraries                  = ""
+-- ----------------------------------------------------------------------------
+-- Port selection
+-- ----------------------------------------------------------------------------
+wx.gtk                          = 0
+wx.motif                        = 0
+wx.osx_carbon                   = 0
+wx.osx_cocoa                    = 0
+wx.osx_iphone                   = 0
+wx.osx                          = 0
+wx.carbon                       = wx.osx_carbon
+wx.cocoa                        = wx.osx_cocoa
+wx.iphone                       = wx.osx_iphone
+wx.mac                          = wx.osx
+wx.old_cocoa                    = 0
+wx.wine                         = 0
+wx.msw                          = 0
+wx.pm                           = 0
+wx.directfb                     = 0
+wx.microwin                     = 0
+wx.x11                          = 0
+wx.nanox                        = 0
+wx.gpe                          = 0
+wx.universal                    = false
+wx.themes                       = "all"
+-- ----------------------------------------------------------------------------
+-- External libraries
+-- ----------------------------------------------------------------------------
+wx.libpng                       = 0
+wx.libjpeg                      = 0
+wx.libtiff                      = 0
+wx.libxpm                       = 0
+wx.libiconv                     = 0
+wx.libmspack                    = 0
+wx.gtkprint                     = 0
+wx.gnomeprint                   = 0
+wx.gnomevfs                     = 0
+wx.libnotify                    = 0
+wx.hildon                       = 0
+wx.opengl                       = 0
+
+wx.dmalloc                      = 0
+wx.sdl                          = 0
+wx.regex                        = 0
+wx.zlib                         = 0
+wx.expat                        = 0
+
+wx.macosx-sdk                   = ""
+wx.macosx-version-min           = ""
+-- ----------------------------------------------------------------------------
+-- Debugging options
+-- ----------------------------------------------------------------------------
+wx.debug                        = false
+wx.debug_flag                   = false
+wx.debug_info                   = false
+wx.debug_gdb                    = false
+-- ----------------------------------------------------------------------------
+-- Optional non GUI features
+-- ----------------------------------------------------------------------------
+wx.intl                         = false
+wx.xlocale                      = false
+wx.config                       = false
+wx.protocols                    = false
+wx.ftp                          = false
+wx.http                         = false
+wx.fileproto                    = false
+wx.sockets                      = false
+wx.ipv6                         = false
+wx.ole                          = false
+wx.dataobj                      = false
+wx.baseevtloop                  = false
+wx.epollloop                    = false
+wx.selectloop                   = false
+
+-- Please keep the settings below in alphabetical order
+wx.any                          = false
+wx.apple_ieee                   = false
+wx.arcstream                    = false
+wx.base64                       = false
+wx.backtrace                    = false
+wx.catch_segvs                  = false
+wx.cmdline                      = false
+wx.datetime                     = false
+wx.debugreport                  = false
+wx.dialupman                    = false
+wx.dynlib                       = false
+wx.dynamicloader                = false
+wx.exceptions                   = false
+wx.ffile                        = false
+wx.file                         = false
+wx.filehistory                  = false
+wx.filesystem                   = false
+wx.fontenum                     = false
+wx.fontmap                      = false
+wx.fs_archive                   = false
+wx.fs_inet                      = false
+wx.fs_zip                       = false
+wx.fswatcher                    = false
+wx.geometry                     = false
+wx.log                          = false
+wx.longlong                     = false
+wx.mimetype                     = false
+wx.printfposparam               = false
+wx.snglinst                     = false
+wx.sound                        = false
+wx.stdpaths                     = false
+wx.stopwatch                    = false
+wx.streams                      = false
+wx.sysoptions                   = false
+wx.tarstream                    = false
+wx.textbuf                      = false
+wx.textfile                     = false
+wx.timer                        = false
+wx.variant                      = false
+wx.zipstream                    = false
+
+-- URL-related classes
+wx.url                          = false
+wx.protocol                     = false
+wx.protocol-http                = false
+wx.protocol-ftp                 = false
+wx.protocol-file                = false
+
+wx.threads                      = false
+
+wx.iniconf                      = false
+wx.regkey                       = false
+-- ----------------------------------------------------------------------------
+-- Optional "big" GUI features
+-- ----------------------------------------------------------------------------
+wx.docview                      = false
+wx.help                         = false
+wx.mshtmlhelp                   = false
+wx.html                         = false
+wx.htmlhelp                     = false
+wx.xrc                          = false
+wx.aui                          = false
+wx.propgrid                     = false
+wx.ribbon                       = false
+wx.stc                          = false
+wx.constraints                  = false
+wx.loggui                       = false
+wx.logwin                       = false
+wx.logdialog                    = false
+wx.mdi                          = false
+wx.mdidoc                       = false
+wx.mediactrl                    = false
+wx.gstreamer8                   = false
+wx.richtext                     = false
+wx.postscript                   = false
+wx.printarch                    = false
+wx.svg                          = false
+wx.webkit                       = false
+wx.webview                      = false
+
+-- wxDC is implemented in terms of wxGraphicsContext in wxOSX so the latter
+-- can't be disabled, don't even provide an option to do it
+wx.graphics_ctx                 = false
+-- ----------------------------------------------------------------------------
+-- IPC &c
+-- ----------------------------------------------------------------------------
+wx.clipboard                    = false
+wx.dnd                          = false
+-- ----------------------------------------------------------------------------
+-- Optional GUI controls
+-- ----------------------------------------------------------------------------
+-- Even with --disable-controls, some may be enabled by an explicit
+-- --enable-<control> later on the command line
+-- but by default all will be disabled
+wx.controls                     = false
+
+-- Features affecting multiple controls
+wx.markup                       = false
+
+-- Please keep the settings below in alphabetical order
+wx.accel                        = false
+wx.animatectrl                  = false
+wx.artstd                       = false
+wx.arttango                     = false
+wx.bannerwindow                 = false
+wx.bmpbutton                    = false
+wx.bmpcombobox                  = false
+wx.button                       = false
+wx.calendar                     = false
+wx.caret                        = false
+wx.checkbox                     = false
+wx.checklst                     = false
+wx.choice                       = false
+wx.choicebook                   = false
+wx.collpane                     = false
+wx.colourpicker                 = false
+wx.combobox                     = false
+wx.comboctrl                    = false
+wx.commandlinkbutton            = false
+wx.dataviewctrl                 = false
+wx.datepick                     = false
+wx.detect_sm                    = false
+wx.dirpicker                    = false
+wx.display                      = false
+wx.editablebox                  = false
+wx.filectrl                     = false
+wx.filepicker                   = false
+wx.fontpicker                   = false
+wx.gauge                        = false
+wx.grid                         = false
+wx.headerctrl                   = false
+wx.hyperlink                    = false
+wx.imaglist                     = false
+wx.infobar                      = false
+wx.listbook                     = false
+wx.listbox                      = false
+wx.listctrl                     = false
+wx.notebook                     = false
+wx.notifmsg                     = false
+wx.odcombobox                   = false
+wx.popupwin                     = false
+wx.radiobox                     = false
+wx.radiobtn                     = false
+wx.richmsgdlg                   = false
+wx.richtooltip                  = false
+wx.rearrangectrl                = false
+wx.sash                         = false
+wx.scrollbar                    = false
+wx.searchctrl                   = false
+wx.slider                       = false
+wx.spinbtn                      = false
+wx.spinctrl                     = false
+wx.splitter                     = false
+wx.statbmp                      = false
+wx.statbox                      = false
+wx.statline                     = false
+wx.stattext                     = false
+wx.statusbar                    = false
+wx.taskbaricon                  = false
+wx.tbarnative                   = false
+wx.textctrl                     = false
+wx.timepick                     = false
+wx.tipwindow                    = false
+wx.togglebtn                    = false
+wx.toolbar                      = false
+wx.toolbook                     = false
+wx.treebook                     = false
+wx.treectrl                     = false
+wx.treelist                     = false
+-- ----------------------------------------------------------------------------
+-- Common dialogs
+-- ----------------------------------------------------------------------------
+wx.commondlg                    = false
+wx.aboutdlg                     = false
+wx.choicedlg                    = false
+wx.coldlg                       = false
+wx.dirdlg                       = false
+wx.filedlg                      = false
+wx.finddlg                      = false
+wx.fontdlg                      = false
+wx.msgdlg                       = false
+wx.numberdlg                    = false
+wx.splash                       = false
+wx.textdlg                      = false
+wx.tipdlg                       = false
+wx.progressdlg                  = false
+wx.wizarddlg                    = false
+-- ----------------------------------------------------------------------------
+-- Misc GUI options
+-- ----------------------------------------------------------------------------
+wx.menus                        = false
+wx.miniframe                    = false
+wx.tooltips                     = false
+wx.splines                      = false
+wx.mousewheel                   = false
+wx.validators                   = false
+wx.busyinfo                     = false
+wx.hotkey                       = false
+wx.joystick                     = false
+wx.metafiles                    = false
+wx.dragimage                    = false
+wx.accessibility                = false
+wx.uiactionsim                  = false
+wx.dctransform                  = false
+wx.webviewwebkit                = false
+-- ----------------------------------------------------------------------------
+-- Support for image formats that do not rely on external library
+-- ----------------------------------------------------------------------------
+wx.palette                      = false
+wx.image                        = false
+wx.gif                          = false
+wx.pcx                          = false
+wx.tga                          = false
+wx.iff                          = false
+wx.pnm                          = false
+wx.xpm                          = false
+wx.ico_cur                      = false
+-- ----------------------------------------------------------------------------
+-- wxMSW-only options
+-- ----------------------------------------------------------------------------
+wx.dccache                      = false
+wx.ps-in-msw                    = false
+wx.ownerdrawn                   = false
+wx.uxtheme                      = false
+wx.wxdib                        = false
+wx.webviewie                    = false
+-- This one is not really MSW-specific but it exists mainly to be turned off
+-- under MSW, it should be off by default on the other platforms
+wx.autoidman                    = false
