e042eba37384f3ac3384397efbe9017326066ec5
jgmdev <jgmdev@gmail.com>
2013-01-05 22:44:32 -0400

    Added os.sizeof to premake.

--- build/premake/4.4/src/host/premake.h
+++ build/premake/4.4/src/host/premake.h
@@ -68,6 +68,7 @@ int os_mkdir(lua_State* L);
 int os_pathsearch(lua_State* L);
 int os_rmdir(lua_State* L);
 int os_stat(lua_State* L);
+int os_sizeof(lua_State* L);
 int os_uuid(lua_State* L);
 int string_endswith(lua_State* L);
 
