50f29967c56512f603478d566b67965232866e05
AZ <wxguidesigner@gmail.com>
2013-01-09 04:59:20 +0100

    Renamed libs directory.

--- build/premake/wx/libs.lua
+++ build/premake/wx/libs.lua
@@ -9,32 +9,32 @@
 -- Licence:     wxWindows licence
 -- ============================================================================
 
-require "wx.libs.base"
+require "wx.lib.base"
 
-if wx.debugreport then   require "wx.libs.qa" end
+if wx.debugreport then   require "wx.lib.qa" end
 
-if wx.gui then           require "wx.libs.core"
---  if     wx.gtk   then require "wx.libs.gtk"
---  elseif wx.msdos then require "wx.libs.msdos"
---  elseif wx.msw   then require "wx.libs.msw"
---  elseif wx.os2   then require "wx.libs.os2"
---  elseif wx.osx   then require "wx.libs.osx"
---  elseif wx.univ  then require "wx.libs.univ"
---  elseif wx.x11   then require "wx.libs.x11"
+if wx.gui then           require "wx.lib.core"
+--  if     wx.gtk   then require "wx.lib.gtk"
+--  elseif wx.msdos then require "wx.lib.msdos"
+--  elseif wx.msw   then require "wx.lib.msw"
+--  elseif wx.os2   then require "wx.lib.os2"
+--  elseif wx.osx   then require "wx.lib.osx"
+--  elseif wx.univ  then require "wx.lib.univ"
+--  elseif wx.x11   then require "wx.lib.x11"
 --  end
 end
 
-if wx.zlib  == "builtin" then require "wx.libs.zlib"  end
-if wx.expat == "builtin" then require "wx.libs.expat" end
-if wx.regex == "builtin" then require "wx.libs.regex" end
+if wx.zlib  == "builtin" then require "wx.lib.zlib"  end
+if wx.expat == "builtin" then require "wx.lib.expat" end
+if wx.regex == "builtin" then require "wx.lib.regex" end
 
 if wx.gui and wx.image then
-    if wx.libpng  == "builtin" then require "wx.libs.png"  end
-    if wx.libjpeg == "builtin" then require "wx.libs.jpeg" end
-    if wx.libtiff == "builtin" then require "wx.libs.tiff" end
+    if wx.libpng  == "builtin" then require "wx.lib.png"  end
+    if wx.libjpeg == "builtin" then require "wx.lib.jpeg" end
+    if wx.libtiff == "builtin" then require "wx.lib.tiff" end
 end
 
-if wx.aui      then require "wx.libs.aui"      end
-if wx.propgrid then require "wx.libs.propgrid" end
-if wx.ribbon   then require "wx.libs.ribbon"   end
-if wx.stc      then require "wx.libs.stc"      end
+if wx.aui      then require "wx.lib.aui"      end
+if wx.propgrid then require "wx.lib.propgrid" end
+if wx.ribbon   then require "wx.lib.ribbon"   end
+if wx.stc      then require "wx.lib.stc"      end
